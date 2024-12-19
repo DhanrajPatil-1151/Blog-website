f1cd7f7b0400db8d030dd44c1e47005aa2ce799d
Shivraj1150 <shivrajpatil21517@gmail.com>
2024-12-19 17:47:25 +0530

    Created a landing page for blog site

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional smooth scroll for better navigation experience
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
